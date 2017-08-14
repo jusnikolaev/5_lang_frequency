a0269f2e2b9df9e475ab0617db805597f06c1bae
Iurii Nikolaev <jusnikolaev@gmail.com>
2017-08-14 23:15:07 +0300

    Refactoring

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,14 +4,18 @@ import os
 import argparse
 
 
-def load_data(file_path):
+def valiate_path(file_path):
     if not os.path.exists(file_path):
         print('No such file in directory')
-        return None
+        return False
     else:
-        with open(file_path) as file:
-            text_file = file.read()
-            return text_file
+        return True
+
+
+def load_data(file_path):
+    with open(file_path) as file:
+        text_file = file.read()
+        return text_file
 
 
 def delete_punctuation(text):
@@ -20,25 +24,24 @@ def delete_punctuation(text):
     return text.translate(text_without_punctation).split()
 
 
-def get_most_frequent_words(list_of_words):
+def get_most_frequent_words(list_of_words, limit_of_words):
     counter = Counter(list_of_words)
     popular_words = counter.most_common()
-    return popular_words
+    return popular_words[0:limit_of_words]
 
 
 def show_word(popular_words):
-    output_result_limit = 10
-    for word in range(output_result_limit):
-        print('{}. {} -> {}'.format(word + 1,
-                                    popular_words[word][0],
-                                    popular_words[word][1]))
+    for word in popular_words:
+        print(word)
+        print('{} -> {}'.format(word[0], word[1]))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='File path')
     parser.add_argument('--path', type=str)
     args = parser.parse_args()
-    file_with_text = load_data(args.path)
-    text_without_punctation = delete_punctuation(file_with_text)
-    show_word(get_most_frequent_words(text_without_punctation))
+    if valiate_path(args.path):
+        file_with_text = load_data(args.path)
+        text_without_punctation = delete_punctuation(file_with_text)
+        show_word(get_most_frequent_words(text_without_punctation, 9))
 
